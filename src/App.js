c73ccc1de946924b4d8a699c67664f58942f9dfb
Shyam70233 <deluxshop468@gmail.com>
2022-05-28 10:45:49 +0530

    Hook 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.css';
 
 export default function App() {
+  const [a, seta] = useState([]);
+
+  const load = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((d) => seta(d.data));
+  };
+
+  useEffect(load, []);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {a.map((d) => (
+        <h4>{d.id}</h4>
+      ))}
     </div>
   );
 }
